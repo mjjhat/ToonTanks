c36790a7eec77b32dd8d7f1564c82fb3f93bdef9
mjjhat <63317563+mjjhat@users.noreply.github.com>
2021-09-01 15:15:58 -0600

    Still trying

--- Toontanks/Source/Toontanks/Toontanks.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Toontanks.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Toontanks, "Toontanks" );
