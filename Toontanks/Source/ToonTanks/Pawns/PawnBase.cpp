6fcef771ec2afe6eefbe8c2dd15aa69650d6d84e
mjjhat <63317563+mjjhat@users.noreply.github.com>
2021-09-02 17:30:52 -0600

    5.135: Creatng Components Complete

--- Toontanks/Source/ToonTanks/Pawns/PawnBase.cpp
+++ Toontanks/Source/ToonTanks/Pawns/PawnBase.cpp
@@ -2,6 +2,7 @@
 
 
 #include "ToonTanks/Pawns/PawnBase.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 APawnBase::APawnBase()
@@ -9,21 +10,32 @@ APawnBase::APawnBase()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-/* The Create Default Subobject is a Function will be create and
+/* QUICKLY COMPILE: The Create Default Subobject is a Function will be create and
 add the component (whatever component we want to set) to the inside of our 
-existing class.*/
+existing class.
 
-/* <> are added because the function is expecting a class type, which we declared
+CLASSTYPE EXPECTED: <> are added because the function is expecting a class type, which we declared
 in the .h file, UCapsuleComponents
 
-This is at top because it is the root component, the parent on top from which
+HIERARCHY: This is at top because it is the root component, the parent on top from which
 the rest of the components will stem out from. So stick it at top*/
 
 	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
-// Below is added to clarify what we just did and remove any ambiguity
+// CLARITY: Below is added to clarify what we just did and remove any ambiguity
 	RootComponent = CapsuleComp;
 
 	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+// We want BaseMesh to inherit properly movement and capsule, so:
+	BaseMesh->SetupAttachment(RootComponent);
+/* INHEREIT so we setup the attachment, Root Component
+which is the CaspuleComp, so the BaseMesh can inherit from it.*/
+
+/*NO ROOT: RootComponent is removed because we want it to follow the basemesh of the Tank*/
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+ 	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
+ 	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 }
 
 // Called when the game starts or when spawned
