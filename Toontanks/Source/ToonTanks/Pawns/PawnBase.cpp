7a96e78e1a955b3a252123bbcbb6bc0e7248b896
mjjhat <63317563+mjjhat@users.noreply.github.com>
2021-09-20 15:11:24 -0600

    5.152-4: quick learnings?

--- Toontanks/Source/ToonTanks/Pawns/PawnBase.cpp
+++ Toontanks/Source/ToonTanks/Pawns/PawnBase.cpp
@@ -10,12 +10,16 @@ APawnBase::APawnBase()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-/* QUICKLY COMPILE: The Create Default Subobject is a Function will be create and
+
+/***********Below will allow these components to show up in a BP file****************/
+
+
+/* QUICKLY COMPILE: The CreateDefaultSubobject<> is a Function will be create and
 add the component (whatever component we want to set) to the inside of our 
 existing class.
 
 CLASSTYPE EXPECTED: <> are added because the function is expecting a class type, which we declared
-in the .h file, UCapsuleComponents
+in the .h file, <UCapsuleComponents>
 
 HIERARCHY: This is at top because it is the root component, the parent on top from which
 the rest of the components will stem out from. So stick it at top*/
@@ -25,17 +29,17 @@ the rest of the components will stem out from. So stick it at top*/
 	RootComponent = CapsuleComp;
 
 	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
-// We want BaseMesh to inherit properly movement and capsule, so:
+// We want BaseMesh to inherit  movement and capsule properly, so:
 	BaseMesh->SetupAttachment(RootComponent);
 /* INHEREIT so we setup the attachment, Root Component
 which is the CaspuleComp, so the BaseMesh can inherit from it.*/
 
 /*NO ROOT: RootComponent is removed because we want it to follow the basemesh of the Tank*/
 	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
- 	TurretMesh->SetupAttachment(BaseMesh);
+ 	TurretMesh->SetupAttachment(BaseMesh);//follows base mesh, not capsule
 
 	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
- 	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
+ 	ProjectileSpawnPoint->SetupAttachment(TurretMesh);// follows turret mesh
 }
 
 // Called when the game starts or when spawned
--- Toontanks/Source/ToonTanks/Pawns/PawnBase.h
+++ Toontanks/Source/ToonTanks/Pawns/PawnBase.h
@@ -1,27 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-/* includes look for a specific class and all its details, including any
-includes that might be included. This can increase compile times since it
-must search through all that information. It also reads it each time a child
-class might call something that has it attached*/
+	/* includes look for a specific class and all its details, including any
+	includes that might be included. This can increase compile times since it
+	must search through all that information. It also reads it each time a child
+	class might call something that has it attached*/
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "PawnBase.generated.h"
 
-/*Below is a Forward Declaration. It points directly to the information
-you want called up and used for the specific header or .cpp file. When 
-this happens, it does not have to call up all the includes that might be
-included in the class being called up- it only wants what is in that file
-and no other. Thus greatly speeds up compiling time. 
+	/*Below is a Forward Declaration. It points directly to the information
+	you want called up and used for the specific header or .cpp file. When 
+	this happens, it does not have to call up all the includes that might be
+	included in the class being called up- it only wants what is in that file
+	and no other. Thus greatly speeds up compiling time. 
 
-It also specifies what class or variable you are looking to use, and the
-right one if it happens to be used in multiple points
+	It also specifies what class or variable you are looking to use, and the
+	right one if it happens to be used in multiple points
 
-It is like any class declaration, and is called "forward" since it declared
-before used, like any other class. */
-class UCapsuleComponent; 
+	It is like any class declaration, and is called "forward" since it declared
+	before used, like any other class. */
+class UCapsuleComponent; //frwrd declare
 
 
 UCLASS()
@@ -30,37 +30,77 @@ class TOONTANKS_API APawnBase : public APawn
 	GENERATED_BODY()
 
 private:
+	/* It is good to put most of our properites in the private section so that they do not 
+		cross over into other classes. However, we still would like to access them in the 
+		editor. We can do this by adding the "meta..." code as seen below.*/
 
-/* this is the top component in this base pawn's hierarchy, from which the other
-parts will flow out from, nestled underneath
 
-It will also act as a general collider for the Pawns that use this class*/
-	UPROPERTY()
-	UCapsuleComponent* CapsuleComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCapsuleComponent* CapsuleComp;//Top- pointer
+		/* this is the top component in this base pawn's hierarchy, from which the other
+		parts will flow out from, nestled underneath
 
-/* Don't need to forward declare these, unlike above, since the class
-has a basic "understaninding" What a Base and Turret Mesh are.
+		It will also act as a general collider for the Pawns that use this class*/
 
-These are generic since both player and AI will have a base and turret.
-The different will be whether the base will be mobile (Player) or static
-(AI)*/
-	UPROPERTY()
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UStaticMeshComponent* BaseMesh;
-	UPROPERTY()
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UStaticMeshComponent* TurretMesh;
+		/* Don't need to forward declare these, unlike above, since the class
+		has a basic "understaninding" What a Base and Turret Mesh are.
 
-/* tells where the "projectile" spawns, so can be seen in the viewport
-and act accordingly. 
-
-USceneComponent is a collection of data that also includes a transform
-so can have a visual representation of what is spawning in the World. */
-	UPROPERTY()
+		These are generic since both player and AI will have a base and turret.
+		The difference will be whether the base will be mobile (Player) or static
+		(AI)*/
+		
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	USceneComponent* ProjectileSpawnPoint;
+		/* tells where the "projectile" spawns, so can be seen in the viewport
+		and act accordingly. 
+
+		USceneComponent is a collection of data that also includes a transform
+		so can have a visual representation of what is spawning in the World. */
+
+		/*COMPONENTS: Standard visible classes = VisibleAnywhere
+			* since going to be a visual component, we do not need to have any reference of it in 
+				the editor. 
+			* meta =... etc. is added because this component is in the private section. Thus it needs
+				code to access it, otherwise the compiler will say that it cannot compile it due to
+				it being in the private properites section.*/
 
 public:
-	// Sets default values for this pawn's properties
+		// Sets default values for this pawn's properties
 	APawnBase();
 
+
+//************* Below are examples of BP editing code******//
+
+
+/*/VISIBLE: greyed out so that others cannot accidently edit them in the editor- good for debugging
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Demo")
+	int VisibleAnywhere;
+	UPROPERTY(VisibleDefaultsOnly, Category = "Demo")
+	int VisibleDefaultsOnly;
+	UPROPERTY(VisibleInstanceOnly, Category = "Demo")
+	int VisibleInstanceOnly;
+
+//EDIT: All can be edited in the editor. Good for debugging as well. 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Demo")
+	int EditAnywhere;
+	UPROPERTY(EditDefaultsOnly, Category = "Demo")
+	int EditDefaultsOnly;
+	UPROPERTY(EditInstanceOnly, Category = "Demo")
+	int EditInstanceOnly;
+		//DEFAULTS ONLY: will only show up in Parent or Child of class, not in editor. Only regards the class itself
+		/*INSTANCE ONLY: will replace the Defaults only when an instance of the class or parent is created
+                   			that is, anything placed in the actual map/level.*/
+		//BLUEPRINT READWRITE/READONLY: These will edit the BP functionality, without which is mainly editor functionality. 
+	
+	
+	
+	
+	
+	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
